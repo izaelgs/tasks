ab99ff0bb39f9b4052e31864c6df2d2646ff22d6
izaelgs <izael-123@outlook.com>
2023-06-18 12:32:06 -0300

    atualiza endpoints category
    adiciona metodos index, get e update

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Exceptions\NotFoundException;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CategoryRequest;
 use App\Models\Category;
@@ -15,7 +16,26 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            $categories = Auth::user()->categories()->get();
+
+            if(!count($categories))
+                throw new NotFoundException();
+
+            return response()->json([
+                "data" => $categories,
+            ], 200);
+        } catch (NotFoundException $e) {
+            return response()->json([
+                "error" => "Not Found",
+                "message" => __('model.not_found', ['model' => 'Categoria', 'artigo_definido' => 'a']),
+            ], 404);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "error" => "Erro Inesperado",
+                "message" => __('model.index_error')
+            ], 400);
+        }
     }
 
     /**
@@ -23,22 +43,22 @@ class CategoryController extends Controller
      */
     public function store(CategoryRequest $request)
     {
-        $data = $request->all();
-
-        $data['user_id'] = Auth::user()->id;
-
-        Category::create($data);
-
-        return response()->json([
-            "message" => __('model.create_success', ['model' => 'Categoria', 'artigo_definido' => 'a']),
-        ], 201);
 
         try {
-            //code...
+            $data = $request->all();
+
+            $data['user_id'] = Auth::user()->id;
+
+            $category = Category::create($data);
+
+            return response()->json([
+                "message"   => __('model.create_success', ['model' => 'Categoria', 'artigo_definido' => 'a']),
+                "data"      => $category
+            ], 201);
         } catch (\Throwable $th) {
             return response()->json([
-                "message" => __('model.create_error'),
-                "code" => $th->getMessage()
+                "error"     => __('model.create_error'),
+                "message" => $th->getMessage()
             ], 400);
         }
     }
@@ -48,15 +68,49 @@ class CategoryController extends Controller
      */
     public function show(string $id)
     {
-        //
+        try {
+            $category = Auth::user()->categories()->find($id);
+
+            if(!$category)
+                throw new NotFoundException();
+
+            return response()->json([
+                "data" => $category,
+            ], 200);
+        } catch (NotFoundException $e) {
+            return response()->json([
+                "error" => "Not Found",
+                "message" => __('model.not_found', ['model' => 'Categoria', 'artigo_definido' => 'a'])
+            ], 404);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "error" => "Erro Inesperado",
+                "message" => __('model.index_error', ['model' => 'Categoria', 'artigo_definido' => 'a'])
+            ], 400);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(CategoryRequest $request, string $id)
     {
-        //
+        try {
+            $data = $request->all();
+
+            $category = Auth::user()->categories()->find($id);
+
+            $category->update($data);
+
+            return response()->json([
+                "message" => __('model.update_success', ['model' => 'Categoria', 'artigo_definido' => 'a']),
+            ], 201);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "error" => __('model.update_error'),
+                "message" => $th->getMessage(),
+            ], 400);
+        }
     }
 
     /**
--- lang/pt-BR/model.php
+++ lang/pt-BR/model.php
@@ -3,4 +3,11 @@
 return [
     'create_success' => ':model criad:artigo_definido com sucesso',
     'create_error' => 'Ocorreu um erro ao criar :artigo_definido :model',
+
+    'update_success' => ':model atualizad:artigo_definido com sucesso',
+    'update_error' => 'Ocorreu um erro ao atualizar :artigo_definido :model',
+
+    'index_error' => 'Ocorreu um erro ao consultar :artigo_definido(s) :model(s)',
+
+    'not_found' => ':model(s) não encontrad:artigo_definido(s)',
 ];
